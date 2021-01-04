9eaff0b85c929d4f468d1192ee4b634450564081
Sriram26231 <74018096+Sriram26231@users.noreply.github.com>
2021-01-04 19:18:46 +0000

    Add files via upload

---
+++ Project 24/MatterJSBoilerPlate-master/sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	ball = new Ball(420,240,70,70);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("black");
+
+  ball.display();
+  
+  drawSprites();
+ 
+}
+
+
+
